6f0fc77f3217bef481597e435c9371da4dcbdbc9
StasSytnykov <stas82kak05@gmail.com>
2024-10-28 14:29:02 +0200

    organised routign and added more styles

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -3,14 +3,11 @@ import { Link, Stack } from "expo-router";
 
 export default function NotFoundScreen() {
   return (
-    <>
-      <Stack.Screen options={{ title: "Oops! Not Found" }} />
-      <View style={styles.container}>
-        <Link href="/" style={styles.button}>
-          Go back to Home screen!
-        </Link>
-      </View>
-    </>
+    <View style={styles.container}>
+      <Link href="/" style={styles.button}>
+        Go back to Home screen!
+      </Link>
+    </View>
   );
 }
 
